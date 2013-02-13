86021830a0f470cc484702b9fbd0d83fc280fd58
Dave King <dave.king@mailtrust.com>
2013-02-12 19:33:27 -0500

    The first failing test.
    * I already know that I want my test fixtures to be slicing up
      a collection of colored shape models.
    * I already know what interface I want to create a faceted
      collection: facets.facetCollection(criteriaFn, collection)
      A _criteria function_ is a function that takes a model and
      returns facet data for it.  For example if we are faceting
      by color, it will take a red square and return an id 'red' and
      the title 'Red'.

--- spec/faceter_spec.js
+++ spec/faceter_spec.js
@@ -1,3 +1,41 @@
 var assert = require('assert'),
     facets = require('facets'),
     Backbone = require('backbone');
+
+describe('faceter', function () {
+
+  var collection, redTriangle, blueTriangle, redSquare;
+
+  function shapeModel(id, color, shape) {
+    return new Backbone.Model({
+      id: id,
+      color: color,
+      shape: shape
+    });
+  }
+
+  beforeEach(function () {
+    redTriangle = shapeModel(1, 'Red', 'Triangle');
+    blueTriangle = shapeModel(2, 'Blue', 'Triangle');
+    redSquare = shapeModel(3, 'Red', 'Square');
+    collection = new Backbone.Collection([redTriangle, blueTriangle, redSquare]);
+  });
+
+  it('maps every model to one facet', function () {
+    var trivialCriteria, facetedCollection;
+
+    trivialCriteria = function (model) {
+      return {
+        id: 1,
+        title: 'Trivial'
+      };
+    };
+    facetedCollection = facets.facetCollection(trivialCriteria, collection);
+
+    assert.equal(1, facetedCollection.length);
+
+    var singleFacet = facetedCollection.at(0);
+    assert.equal(3, singleFacet.get('count'));
+    assert.equal('Trivial', singleFacet.get('title'));
+  });
+});
